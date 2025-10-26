a3722d184c6f87cb3e9fdf30869abb43da954d8e
DoThucFPTYH <dotrithucnknd@gmail.com>
2025-10-27 05:23:04 +0700

    SQL recursive + fix bug home page

--- src/java/controller/request/ListController.java
+++ src/java/controller/request/ListController.java
@@ -5,28 +5,38 @@
 package controller.request;
 
 import controller.iam.BaseRequiredAuthorizationController;
+import dal.RequestForLeaveDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import model.RequestForLeave;
 import model.iam.User;
 
 /**
  *
- * @author admin
+ * @author sonnt
  */
-@WebServlet(urlPatterns="/request/list")
-public class ListController extends BaseRequiredAuthorizationController{
+@WebServlet(urlPatterns = "/request/list")
+public class ListController extends BaseRequiredAuthorizationController {
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
+        RequestForLeaveDBContext db = new RequestForLeaveDBContext();
+        ArrayList<RequestForLeave> rfls = db.getByEmployeeAndSubodiaries(user.getId());
+        req.setAttribute("rfls", rfls);
+        req.getRequestDispatcher("../view/request/list.jsp").forward(req, resp);
+    }
 
     @Override
     protected void processPost(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        processRequest(req, resp, user);
     }
 
     @Override
     protected void processGet(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        processRequest(req, resp, user);
     }
-    
+
 }
